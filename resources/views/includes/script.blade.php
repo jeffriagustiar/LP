88ccf4d7bfdc89fa30f5ee2e1206a319f054d37f
jeffriagustiar <jeffriagustiar@gmail.com>
2022-03-26 10:46:23 +0700

    slicing template

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,43 @@
+<script src="{{ url('/assets/js/jquery-3.5.1.min.js') }}"></script>
+    <!-- feather icon js-->
+    <script src="{{ url('/assets/js/icons/feather-icon/feather.min.js') }}"></script>
+    <script src="{{ url('/assets/js/icons/feather-icon/feather-icon.js') }}"></script>
+    <!-- Sidebar jquery-->
+    <script src="{{ url('/assets/js/sidebar-menu.js') }}"></script>
+    <script src="{{ url('/assets/js/config.js') }}"></script>
+    <!-- Bootstrap js-->
+    <script src="{{ url('/assets/js/bootstrap/popper.min.js') }}"></script>
+    <script src="{{ url('/assets/js/bootstrap/bootstrap.min.js') }}"></script>
+    <!-- Plugins JS start-->
+    <script src="{{ url('/assets/js/chart/chartist/chartist.js') }}"></script>
+    <script src="{{ url('/assets/js/chart/chartist/chartist-plugin-tooltip.js') }}"></script>
+    <script src="{{ url('/assets/js/chart/knob/knob.min.js') }}"></script>
+    <script src="{{ url('/assets/js/chart/knob/knob-chart.js') }}"></script>
+    <script src="{{ url('/assets/js/chart/apex-chart/apex-chart.js') }}"></script>
+    <script src="{{ url('/assets/js/chart/apex-chart/stock-prices.js') }}"></script>
+    <script src="{{ url('/assets/js/prism/prism.min.js') }}"></script>
+    <script src="{{ url('/assets/js/clipboard/clipboard.min.js') }}"></script>
+    <script src="{{ url('/assets/js/counter/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ url('/assets/js/counter/jquery.counterup.min.js') }}"></script>
+    <script src="{{ url('/assets/js/counter/counter-custom.js') }}"></script>
+    <script src="{{ url('/assets/js/custom-card/custom-card.js') }}"></script>
+    <script src="{{ url('/assets/js/notify/bootstrap-notify.min.js') }}"></script>
+    <script src="{{ url('/assets/js/vector-map/jquery-jvectormap-2.0.2.min.js') }}"></script>
+    <script src="{{ url('/assets/js/vector-map/map/jquery-jvectormap-world-mill-en.js') }}"></script>
+    <script src="{{ url('/assets/js/vector-map/map/jquery-jvectormap-us-aea-en.js') }}"></script>
+    <script src="{{ url('/assets/js/vector-map/map/jquery-jvectormap-uk-mill-en.js') }}"></script>
+    <script src="{{ url('/assets/js/vector-map/map/jquery-jvectormap-au-mill.js') }}"></script>
+    <script src="{{ url('/assets/js/vector-map/map/jquery-jvectormap-chicago-mill-en.js') }}"></script>
+    <script src="{{ url('/assets/js/vector-map/map/jquery-jvectormap-in-mill.js') }}"></script>
+    <script src="{{ url('/assets/js/vector-map/map/jquery-jvectormap-asia-mill.js') }}"></script>
+    <script src="{{ url('/assets/js/dashboard/default.js') }}"></script>
+    <script src="{{ url('/assets/js/notify/index.js') }}"></script>
+    <script src="{{ url('/assets/js/datepicker/date-picker/datepicker.js') }}"></script>
+    <script src="{{ url('/assets/js/datepicker/date-picker/datepicker.en.js') }}"></script>
+    <script src="{{ url('/assets/js/datepicker/date-picker/datepicker.custom.js') }}"></script>
+    <!-- Plugins JS Ends-->
+    
+    <!-- Theme js-->
+    <script src="{{ url('/assets/js/script.js') }}"></script>
+    <script src="{{ url('/assets/js/theme-customizer/customizer.js') }}"></script>
+    <!-- login js-->
